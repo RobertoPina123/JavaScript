66ecc9cfa880bcb47c469878208673e3a22a0d07
RobertoPina123 <165084242+RobertoPina123@users.noreply.github.com>
2024-04-22 15:44:49 -0300

    ex.13

---
+++ aula12/ex13.js
@@ -0,0 +1,41 @@
+agora = new Date()
+//domingo 0
+//segunda 1
+//terça 2
+//quarta 3
+//quinta 4
+//sexta 5
+//sabado 6
+//domingo 7
+
+diaSemana = agora.getDay() 
+
+console.log(diaSemana)
+
+switch(diaSemana) {
+     
+    case 0:
+        console.log('Domingo')
+    break
+    case 1:
+        console.log('Segunda')
+     break
+     case 2:
+        console.log('Terça')
+     break
+     case 3:
+        console.log('Quarta')
+     break
+     case 4:
+        console.log('Quinta')
+     break
+     case 5:
+        console.log('Sexta')
+     break
+     case 6:
+        console.log('Sabado')
+     break
+    
+     default:console.log('Dia inválido')
+
+}
