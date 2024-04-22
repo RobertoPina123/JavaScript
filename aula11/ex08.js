42252c000eb5acddfae8c45aa6af0a7b00798764
RobertoPina123 <165084242+RobertoPina123@users.noreply.github.com>
2024-04-22 10:15:52 -0300

    FIm da aula dez começo da 11

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var velocidade = 59
+
+console.log(`a velocidade do seu carro é ${velocidade}km/h `)
+
+    if (velocidade > 60) {console.log(`Velocidade ultrapassada: Multado`)}
+
+console.log(`dirija sempre usando o cinto de segurança`)
---
+++ aula11/ex09.js
@@ -0,0 +1,11 @@
+var pais = 'Russia'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+}
+
+else {console.log('Você é estrangeiro')}
+
+
